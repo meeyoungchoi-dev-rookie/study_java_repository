68fde392cd6bfb4f92f72346b4733a6f59f32a93
meeyoungchoi <goodlife1359@gmail.com>
2022-10-10 11:39:36 +0900

    인터페이스 구현 예제 및 정리

---
+++ src/상속/interfaceImpl/Fighter.java
@@ -0,0 +1,16 @@
+package 상속.interfaceImpl;
+
+import 상속.abstractEx.Unit;
+import 상속.interfaceExtends.Fightable;
+
+public class Fighter implements Fightable {
+    @Override
+    public void attack(Unit unit) {
+
+    }
+
+    @Override
+    public void move(int x, int y) {
+
+    }
+}
